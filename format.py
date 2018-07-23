f41d73a07b3c83429bd2aabfd8d0ef7a236f5553
lichenguang <lichenguang@hotmail.com>
2018-07-23 14:44:04 +0800

    optimize auto download code

--- format.py
+++ format.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 import re
 from optparse import OptionParser
-
 from you_get.extractors import youtube
 
 
@@ -21,22 +20,30 @@ def parse():
 
 
 def download(frame, index=0):
-    start_flag = False
-    for i, item in enumerate(frame.values):
-        url = item[2]
-        print(i)
-        print(url)
-        if index == i:
-            start_flag = True
-            continue
-        if not start_flag:
-            continue
-        result = youtube.download(
-            url,
-            output_dir='/data/videos',
-            merge=False
-        )
+    with open('download_index.txt', 'a') as fp:
+        start_flag = False
+        for i, item in enumerate(frame.values):
+            fp.write(str(i) + '\n')
+            url = item[2]
+            print(i)
+            print(url)
+            if index == i:
+                start_flag = True
+                continue
+            if not start_flag:
+                continue
+            result = youtube.download(
+                url,
+                output_dir='/data/videos',
+                merge=False
+            )
 
+
+def get_index():
+    with open('download_index.txt', 'r') as fp:
+        data = fp.readlines()
+    index = int(data[-2])
+    return index
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -44,5 +51,8 @@ if __name__ == '__main__':
                       help="skip index")
     (options, args) = parser.parse_args()
     index = int(options.index)
+    if index == -1:
+        index = get_index()
+
     frame = pd.read_csv('data/crawled_d.csv')
     download(frame, index)
